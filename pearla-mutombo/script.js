0470fc657bc5f00a69c09fc9e233abff85548284
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-04 03:29:21 -0400

    finalized correction on index, css and script.

--- pearla-mutombo/script.js
+++ pearla-mutombo/script.js
@@ -2,7 +2,7 @@
 
 // 1. Using querySelector with a class
 const header = document.querySelector (".header-style");
-header.innerText = "Hello World I'm Pearla! Welcom to my Atlanta-based tech journey.";
+header.innerText = "Hello World I'm Pearla! Welcome to my Atlanta-based tech journey.";
 
 // 2. Using getElementByID with ID
 const bioStatus = document.getElementById("bio-update");
