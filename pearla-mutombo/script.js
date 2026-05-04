d506c55152f3ce287d0a5854dd858e70b4be6fe4
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-04 02:46:31 -0400

    made corrections on index page, css page and script page.

---
+++ pearla-mutombo/script.js
@@ -0,0 +1,9 @@
+// in script.js
+
+// 1. Using querySelector with a class
+const header = document.querySelector (".header-style");
+header.innerText = "Hello World I'm Pearla! Welcom to my Atlanta-based tech journey.";
+
+// 2. Using getElementByID with ID
+const bioStatus = document.getElementById("bio-update");
+bioStatus.innerHTML = "<em>Computing is a whole new world<em>";
