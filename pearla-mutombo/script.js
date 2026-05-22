4fd26f4b419b74312a1294c86271df93966328c2
Pearla Mutombo <pmutombo1986@gmail.com>
2026-05-21 22:45:53 -0400

    added an id on my index.html page and formatted semantic tags.

--- pearla-mutombo/script.js
+++ pearla-mutombo/script.js
@@ -1,8 +1,9 @@
 // in script.js
 
 // 1. Using querySelector with a class
-const header = document.querySelector (".header-style");
-header.innerText = "Hello World I'm Pearla! Welcome to my Atlanta-based tech journey.";
+const header = document.querySelector("header-style");
+header.innerText =
+  "Hello World I'm Pearla! Welcome to my Atlanta-based tech journey.";
 
 // 2. Using getElementByID with ID
 const bioStatus = document.getElementById("bio-update");
