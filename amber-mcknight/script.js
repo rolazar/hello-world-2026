421960295ca3aa83ca72b6339a7a78dc6d34dc0f
Amber <ambermcknight54@yahoo.com>
2026-06-01 23:54:17 -0500

    changes to script. js

--- amber-mcknight/script.js
+++ amber-mcknight/script.js
@@ -1,16 +1,12 @@
 const img1 = document.getElementById("img1");
-
 if (img1) {
-  img1.insertAdjacentHTML("afterend", "AMBER");
+  img1.src = "92ECD092-4FD8-489C-AEBB-152F1E4864D0_1_105_c.jpeg";
 }
 const img2 = document.getElementById("img2");
-
 if (img2) {
-  img2.insertAdjacentHTML("afterend", "PEACE");
+  img2.src = "peace.jpg";
 }
-
 const img3 = document.getElementById("img3");
-
 if (img3) {
-  img3.insertAdjacentHTML("afterend", "CODE CONQUER");
+  img3.src = "conquering code.jpg";
 }
