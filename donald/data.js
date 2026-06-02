20f9ba27d8a4d21f90ab157060f2551f6dea79b9
dwayne ross <donaldwross24@gmail.com>
2026-06-01 20:45:59 -0500

    json completed

---
+++ donald/data.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const colors = ["red", "black", "blue", "yellow", "green"];
+
+console.log(colors);
+
+const helloHead1 = document.getElementById("hello");
+
+helloHead1.innerHTML = "Welcome to Donald's World";
