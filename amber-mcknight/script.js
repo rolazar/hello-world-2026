99eb7d2d834edeed32a291385818da06d411cc4c
Amber <ambermcknight54@yahoo.com>
2026-06-05 22:37:08 -0500

    changed javascript to fit the page

--- amber-mcknight/script.js
+++ amber-mcknight/script.js
@@ -1,30 +1,7 @@
-const formTag = document.getElementById("imageFormId");
-formTag.onsubmit = handleSubmit;
+const captionOne = document.getElementById("cap1");
+const captionTwo = document.getElementById("cap2");
+const captionThree = document.getElementById("cap3");
 
-const cap1 = document.getElementById("cap1");
-const cap2 = document.getElementById("cap2");
-const cap3 = document.getElementById("cap3");
-
-function handleSubmit(event) {
-  event.preventDefault();
-  const form = event.target;
-  const isChecked1 = form.elements.checkbox1.checked;
-  const isChecked2 = form.elements.checkbox2.checked;
-  const isChecked3 = form.elements.checkbox3.checked;
-
-  if (isChecked1) {
-    cap1.innerText = "Amber";
-  } else {
-    cap1.innerText = "";
-  }
-  if (isChecked2) {
-    cap2.innerText = "Vision of Peace";
-  } else {
-    cap2.innerText = "";
-  }
-  if (isChecked3) {
-    cap3.innerText = "Computers";
-  } else {
-    cap3.innerText = "";
-  }
-}
+captionOne.innerText = "Amber";
+captionTwo.innerText = "Vision of Peace";
+captionThree.innerText = "Conquering Code";
